6113c6900c307997fc2c3870c3b5fbbc8ac7f7f8
仇灾幼 <26304262+QIUZAIYOU@users.noreply.github.com>
2023-07-24 08:15:03 +0800

    Add files via upload

---
+++ serverchen.py
@@ -0,0 +1,14 @@
+import urllib.parse
+import urllib.request
+
+
+def sc_send(text, desp='', key='SCT49296TkTu3IvpwrzlTKgjAy0jfrXLm'):
+    postdata = urllib.parse.urlencode({
+        'text': text,
+        'desp': desp
+    }).encode('utf-8')
+    url = f'https://sctapi.ftqq.com/{key}.send'
+    req = urllib.request.Request(url, data=postdata, method='POST')
+    with urllib.request.urlopen(req) as response:
+        result = response.read().decode('utf-8')
+    return result
