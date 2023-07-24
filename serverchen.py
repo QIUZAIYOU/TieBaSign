e658960b02b7dcbb4233682f1d787633728b1a9a
仇灾幼 <26304262+QIUZAIYOU@users.noreply.github.com>
2023-07-24 08:17:19 +0800

    Update serverchen.py

--- serverchen.py
+++ serverchen.py
@@ -1,14 +1,20 @@
+import os
 import urllib.parse
 import urllib.request
 
+server_chen_key = os.environ['SERVERCHENKEY']
+def sc_send(text, desp='', key=server_chen_key):
+    try:
+        postdata = urllib.parse.urlencode({
+            'text': text,
+            'desp': desp
+        }).encode('utf-8')
+        url = f'https://sctapi.ftqq.com/{key}.send'
+        req = urllib.request.Request(url, data=postdata, method='POST')
+        with urllib.request.urlopen(req) as response:
+            result = response.read().decode('utf-8')
+        return result
+    except Exception as e:
+        logger.error(f"server酱发送失败:{e}")
 
-def sc_send(text, desp='', key='SCT49296TkTu3IvpwrzlTKgjAy0jfrXLm'):
-    postdata = urllib.parse.urlencode({
-        'text': text,
-        'desp': desp
-    }).encode('utf-8')
-    url = f'https://sctapi.ftqq.com/{key}.send'
-    req = urllib.request.Request(url, data=postdata, method='POST')
-    with urllib.request.urlopen(req) as response:
-        result = response.read().decode('utf-8')
-    return result
+sc_send("Github｜TieBaSign｜所有用户签到结束")
