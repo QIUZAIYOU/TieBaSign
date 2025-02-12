6be1ebb0ccec5f2580975cdfb9a2bb41581dd8f3
QIAN <26304262+QIUZAIYOU@users.noreply.github.com>
2025-02-12 11:23:03 +0800

    Update meow.py

--- meow.py
+++ meow.py
@@ -3,8 +3,8 @@ from urllib.parse import urlencode, quote
 from urllib.request import Request, urlopen
 from urllib.error import URLError, HTTPError
 
-meow_key = os.environ['MEOW']
-API_BASE_URL = f'http://api.chuckfang.com/{meow_key}/'
+meow_name = os.environ['MEOW_NAME']
+API_BASE_URL = f'http://api.chuckfang.com/{meow_name}/'
 
 def meow_send(title='', message=''):
     encoded_title = quote(title)
